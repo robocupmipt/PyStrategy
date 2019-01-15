8b1a8a23aa715bc70013dd134956c84b25798bd9
dimakarp1996 <dimakarp1996@yandex.ru>
2019-01-15 18:13:17 +0300

    Update CVConnector.py

--- CVConnector.py
+++ CVConnector.py
@@ -6,8 +6,7 @@ import time
 
 class CVConnector(object):
     def __init__(self, ip, 
-                 ballfinder_dirs=['top_cascade.xml','bottom_cascade.xml',
-                 'other_cascade.xml'],
+                 ballfinder_dirs=['top_cascade.xml','other_cascade.xml','bottom_cascade.xml'],
                  camera_id=1):
         '''
         ballfinder_dir - directory of classifier
@@ -22,42 +21,47 @@ class CVConnector(object):
         self.camera_id = camera_id
         self.last_image=None
 
-    def get_image(self):
+    def get_image(self, image_dir=None):
         '''
         yaw is a horisontal angle ( -2.0857 to 2.0857 )
         pitch is a vertical angle ( -0.6720 to 0.5149)
         speed is a speed of head rotation ( from 0 to 1)
+        num_balls = number of balls
         Note that max abs of pitch value depends on the max abs of yaw value
         http://doc.aldebaran.com/1-14/family/robots/joints_robot.html
         '''
         #ses = qi.Session()
         #ses.connect(self.IP)
         #video = ses.service('ALVideoDevice')
-        video = ALProxy('ALVideoDevice', self.IP, self.PORT)
-        videoClient = video.subscribeCamera("python_client",
-                                            self.camera_id, 2, 11, 5)
-
-        naoImage = video.getImageRemote(videoClient)
-        video.unsubscribe(videoClient)
-        imageWidth = naoImage[0]
-        imageHeight = naoImage[1]
-        array = naoImage[6]
-        im = Image.frombytes("RGB", (imageWidth, imageHeight), str(array))
-        self.last_image = cv2.cvtColor(np.asarray(im), cv2.COLOR_RGB2BGR)
+        if image_dir:
+            self.last_image = cv2.imread(image_dir)
+        else:
+            video = ALProxy('ALVideoDevice', self.IP, self.PORT)
+            videoClient = video.subscribeCamera("python_client",
+                                                self.camera_id, 2, 11, 5)
+        
+            naoImage = video.getImageRemote(videoClient)
+            video.unsubscribe(videoClient)
+            imageWidth = naoImage[0]
+            imageHeight = naoImage[1]
+            array = naoImage[6]
+            im = Image.frombytes("RGB", (imageWidth, imageHeight), str(array))
+            self.last_image = cv2.cvtColor(np.asarray(im), cv2.COLOR_RGB2BGR)
 
     def _get_ball(self, get_image=True,
                   scale_factor=1,
                   haar_params=(1.3, 5),
                   save_image=False,
                   save_dir='DETECTED_BALL.jpg',
-                  print_=False):
+                  print_=False,
+                  num_balls=1):
         '''
         if get_image=True, we make new image, with parameters yaw,pitch,speed
         scale_factor is a parameter on which we rescale image before detect
         haar_params are params for ball finder
         if save_image=True, we save image after detection in save_dir.
         '''
-        if get_image or not self.last_image:
+        if get_image or self.last_image is not None:
             self.get_image()
 
         try:
@@ -69,38 +73,23 @@ class CVConnector(object):
             if print_:
                 print('No image received')
             return None
-
-        try:
-            balls = [scale_factor*ballfinder.detectMultiScale(
+        balls=[]
+        for ballfinder in self.ballfinders:
+            balls_found = scale_factor*ballfinder.detectMultiScale(
                     image1, haar_params[0],haar_params[1])
-                    for ballfinder in self.ballfinders]
-            balls=[j for j in balls if len(j)>0]
-            ans=[[0,0,0,0]]
-            for ball in balls:
-                for i in range(4):
-                    ans[0][i]+=ball[i]
-            for i in range(4):
-                ans[0][i]=ans[0][i]//len(balls)
-            #print(balls)
-        except:
-            if print_:
-                print('Exception while applying cascade')
-                #raise Exception
-            return None
-
-        for (x, y, w, h) in balls:
-            image1 = cv2.rectangle(image1, (x, y),
-                                   (x+w, y+h),
-                                   (255, 0, 0), 2)
-
+            if len(balls)==0:
+                balls=balls_found
+        balls=balls[:num_balls]
+        for (x,y,w,h) in balls:
+            image1 = cv2.rectangle(image1, (x,y),(x+w,y+h),(255,0,0),2)
         if save_image:
             cv2.imwrite(save_dir, image1)
-
         if len(balls)==0:
             if print_:
                 print('No balls found - returning empty')
                 #raise Exception
             return None
+        
         return balls
 
     def get_ball_center(self):
@@ -108,8 +97,7 @@ class CVConnector(object):
         if ball_coords is None or len(ball_coords) == 0:
             cv2.imwrite("images/BAD_IMAGE_{}.jpg".format(time.time()), self.last_image)
             return None, None
-
-        x, y, w, h = ball_coords[0]
+        x, y, w, h = ball_coords
         center_x = x + w//2
         center_y = y + h//2
         return center_x, center_y
@@ -119,7 +107,7 @@ class CVConnector(object):
 
         if ball_coords is None or len(ball_coords)==0:#no balls
             return [None, None]
-        x, y, w, h = ball_coords[0]
+        x, y, w, h = ball_coords
         return w, h
 
     def is_see_ball(self):
