72bb8f554bdd3b217fab860cdf1e284fa558ca25
kozinov.av <kozinov.av@phystech.edu>
2019-01-15 13:11:00 +0300

    basic stratagy

--- main.py
+++ main.py
@@ -6,14 +6,31 @@ class Strategy(object):
     def __init__(self, IP):
         self.motion = MovementConnector(IP)
         self.cv = CVConnector(IP)
-
+        self.FIRST_MAGIC_CONST = 1
+        self.SECOND_MAGIC_CONST = 0.3
 
     def start(self):
-        while not self.cv.is_see_ball();
+        self.motion.SetHeadHorizontalAngle(0)
+        self.motion.SetHeadVerticalAngle(-29.5)
+        while True:
+            while not self.cv.is_see_ball():
+                self.motion.Move(2, 0, 0)
+
+            w, h = self.cv.get_ball_size()
+            while w * h >= self.FIRST_MAGIC_CONST:
+                self.motion.Move(0.5, 0, 0)
+
+            x, y = self.cv.get_ball_center()
+            self.motion.Move(self.SECOND_MAGIC_CONST, 0, 0)
+
+            if x >= 0:
+                self.motion.RightKick()
+            else:
+                self.motion.LeftKick()
+
 
     def head_to_ball(self):
-
-
+        pass
 
 
     def try_find_ball(self):
@@ -26,6 +43,7 @@ class Strategy(object):
 
             if self.cv.is_see_ball():
                 return True
+
 
 def main():
     pass
